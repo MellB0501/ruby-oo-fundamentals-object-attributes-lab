760fc1ef62138bbda0d38ee0c75daf86ca3684e7
Mell Basham <mellbasham@gmail.com>
2020-12-15 14:44:03 -0700

    commit tree a353d73b7c29d8841870563ea4bae831e72ab7b2

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name=(name_dog)
+    @name = name_dog
+    end
+
+    def name
+    @name
+    end
+
+    def breed=(dog_breed)
+    @breed = dog_breed
+    end
+
+    def breed
+    @breed
+    end
+
+end
